45bbdb12a0b5b16d0ecd1d796057e56f7a1f4c65
Randy West <randywest55@gmail.com>
2021-04-19 13:38:16 -0700

    Formatting

--- python/outdatedplugins.py
+++ python/outdatedplugins.py
@@ -28,10 +28,10 @@ def check_for_updates():
     calculate_updates_commands = []
 
     for plug in g_plugs.values():
-        update_commands.append(
-            "git -C %s remote update > /dev/null" % plug["dir"])
+        update_commands.append("git -C %s remote update > /dev/null" % plug["dir"])
         calculate_updates_commands.append(
-            "git -C %s rev-list HEAD..origin/%s --count" % (plug["dir"], plug["branch"]))
+            "git -C %s rev-list HEAD..origin/%s --count" % (plug["dir"], plug["branch"])
+        )
 
     update_commands.append("wait")
     update_command = " & ".join(update_commands)
@@ -40,13 +40,12 @@ def check_for_updates():
     subprocess.run(["bash", "-c", update_command])
 
     out = subprocess.run(
-        ["bash", "-c", calculate_updates_command], stdout=subprocess.PIPE)
+        ["bash", "-c", calculate_updates_command], stdout=subprocess.PIPE
+    )
 
     plugs_to_update = sum(1 for i in out.stdout.decode().split() if int(i) > 0)
-    g_outdated_plugins_silent_mode = int(
-        vim.eval("g:outdated_plugins_silent_mode"))
-    g_outdated_plugins_trigger_mode = int(vim.eval(
-        "g:outdated_plugins_trigger_mode"))
+    g_outdated_plugins_silent_mode = int(vim.eval("g:outdated_plugins_silent_mode"))
+    g_outdated_plugins_trigger_mode = int(vim.eval("g:outdated_plugins_trigger_mode"))
     if plugs_to_update > 0:
         print("Plugins to update: %d" % plugs_to_update)
         if g_outdated_plugins_trigger_mode:
